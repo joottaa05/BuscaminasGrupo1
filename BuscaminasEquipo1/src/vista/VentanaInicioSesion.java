aafd0531f45019cdb78dbb9dd7a4f8e82803b1f0
ikasle <ikasle@DAM1-09.arangoya.local>
2025-03-28 13:24:54 +0100

    Version: 0.0.1

--- BuscaminasEquipo1/src/vista/VentanaInicioSesion.java
+++ BuscaminasEquipo1/src/vista/VentanaInicioSesion.java
@@ -6,15 +6,26 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JToggleButton;
+import javax.swing.JComboBox;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class VentanaInicioSesion extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
 
-	/**
-	 * Launch the application.
-	 */
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -28,16 +39,88 @@ public class VentanaInicioSesion extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+
 	public VentanaInicioSesion() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 334, 184);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(252, 235, 184));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 87, 66, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblNewLabel_2 = new JLabel("Buscaminas");
+		lblNewLabel_2.setForeground(new Color(66, 40, 6));
+		lblNewLabel_2.setBackground(Color.WHITE);
+		lblNewLabel_2.setFont(new Font("Segoe UI", Font.BOLD, 18));
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.gridwidth = 3;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 0;
+		contentPane.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		JLabel lblNewLabel = new JLabel("Usuario:");
+		lblNewLabel.setForeground(new Color(66, 40, 6));
+		lblNewLabel.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 1;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		textField.setForeground(new Color(66, 40, 6));
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 1;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Dificultad:");
+		lblNewLabel_1.setForeground(new Color(66, 40, 6));
+		lblNewLabel_1.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 2;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JComboBox comboBox = new JComboBox<>(new String[] { "Fácil", "Medio", "Difícil" });
+		comboBox.setBackground(Color.WHITE);
+		comboBox.setForeground(new Color(66, 40, 6));
+		comboBox.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridy = 2;
+		contentPane.add(comboBox, gbc_comboBox);
+		
+		JButton btnNewButton = new JButton("Jugar");
+		btnNewButton.setForeground(new Color(66, 40, 6));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridwidth = 3;
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 3;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
 	}
 
 }
