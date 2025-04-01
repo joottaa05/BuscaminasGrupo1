d64569c4d3fceb46d9d3610e53d69a2b6afb5459
ikasle <ikasle@DAM1-06.arangoya.local>
2025-04-01 14:21:18 +0200

    version:0.0.5

---
+++ BuscaminasEquipo1/src/modelo/Casilla.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Casilla {
+
+	private boolean casilla;
+
+	public Casilla(boolean casilla) {
+		this.casilla = casilla;
+	}
+	
+	
+}
--- BuscaminasEquipo1/src/modelo/Tablero.java
+++ BuscaminasEquipo1/src/modelo/Tablero.java
@@ -8,11 +8,26 @@ public class Tablero {
 	}
 	
 	
-	public void generarMinas(Dificultad dificultad) {
+	public String[][] generarTablero(Dificultad dificultad) {
+		int filas = dificultad.getFilas();
+		int columnas = dificultad.getFilas();
+		int minas = dificultad.getminas();
 		
-		for(int minas = dificultad.getminas(); minas>0; minas--) {
+		final int min = 0;
+		final int max = columnas * filas;
+		
+		String[][] tablero = new String[columnas][filas];
+		
+		
+		while(minas > 0) {
+			int mina = (int) (Math.floor(Math.random() * (max - min + 1)) + min);
+			
+			int x = mina % dificultad.getColumnas();
+			int y = (int)(Math.floor(mina / dificultad.getColumnas()));
+			
 			
 		}
+		return tablero;
 	}
 	
 	
