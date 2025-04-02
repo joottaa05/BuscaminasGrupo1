c2f1d36bc8c331752596b18aaec1d73277dd613e
nonam <nonam@192.168.1.85>
2025-04-02 19:57:40 +0200

    Versión: 0.0.6

--- BuscaminasEquipo1/src/controlador/Main.java
+++ BuscaminasEquipo1/src/controlador/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Tablero tablero = new Tablero(Dificultad.Intermedio);
+		Tablero tablero = new Tablero(Dificultad.Dificil);
 
 		tablero.generarTablero();
 		tablero.colocarMinas(tablero.getTablero());
