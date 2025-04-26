e8c66eab3cc22c61adf0b6a31ecd7ead1f9001a0
nonam <nonam@192.168.1.85>
2025-04-26 21:03:50 +0200

    Version: 2.7.0

--- BuscaminasEquipo1/src/vista/VentanaJuego.java
+++ BuscaminasEquipo1/src/vista/VentanaJuego.java
@@ -3,6 +3,7 @@ package vista;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -31,8 +32,17 @@ public class VentanaJuego extends JFrame {
 	private JPanel contentPanel;
 	private JPanel panelSuperior;
 	private JPanel panelInferior;
+	private JPanel panelMinas;
+	private JPanel panelReset;
+	private JPanel panelTemporizador;
 	private JLabel numeroMinas;
 	private JLabel temporizador;
+	private JLabel decenasMinas;
+	private JLabel unidadesMinas;
+	private int filas;
+	private int columnas;
+	private int minasRestantes;
+	private GridBagConstraints gbc;
 	private Color bordes = new Color(134, 96, 67);
 	private JButton[][] tableroInterfaz;
 	private Tablero tablero;
@@ -44,79 +54,62 @@ public class VentanaJuego extends JFrame {
 		setSize(600, 600);
 		setLocationRelativeTo(null);
 
-		
+		this.minasRestantes = dificultad.getminas();
+
 		contentPanel = new JPanel();
 		contentPanel.setLayout(new BorderLayout());
-		contentPanel.setBorder(new LineBorder(bordes,4));
+		contentPanel.setBorder(new LineBorder(bordes, 4));
 		setContentPane(contentPanel);
-		
+
 		panelSuperior = new JPanel();
-		panelSuperior.setPreferredSize(new Dimension(200,50));
+		panelSuperior.setPreferredSize(new Dimension(200, 50));
 		panelSuperior.setBackground(bordes);
-		panelSuperior.setLayout(new GridBagLayout());
-		
+		panelSuperior.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		panelMinas = new JPanel();
+		panelMinas.setLayout(new GridLayout(1, 2));
+		panelSuperior.add(panelMinas);
+
 		panelInferior = new JPanel();
 		panelInferior.setLayout(new GridBagLayout());
-		
+
 		contentPanel.add(panelSuperior, BorderLayout.NORTH);
 		contentPanel.add(panelInferior, BorderLayout.CENTER);
-
-		tablero.generarTablero();
-		tablero.colocarMinas(tablero.getTablero());
-		int filas = tablero.getTablero().length;
-		int columnas = tablero.getTablero()[0].length;
-		tableroInterfaz = new JButton[filas][columnas];
-
-		GridBagConstraints gbc = generarEstructuraTablero();
 		
-		for (int i = 0; i < filas; i++) {
-			for (int j = 0; j < columnas; j++) {
-				tableroInicial(i, j);
 
-				final int x = i;
-				final int y = j;
+		int unidades = this.minasRestantes % 10;
+		int decenas = this.minasRestantes / 10;
+		
+		decenasMinas = new JLabel();
+		actualizarContadorMinas(decenasMinas, decenas);
+		panelSuperior.add(decenasMinas);
+		
+		unidadesMinas = new JLabel();
+		actualizarContadorMinas(unidadesMinas, unidades);
+		panelSuperior.add(unidadesMinas);
 
-				tableroInterfaz[i][j].addMouseListener(new MouseAdapter() {
-					public void mouseClicked(MouseEvent e) {
-						if (SwingUtilities.isLeftMouseButton(e)) {
-							int numero = tablero.conseguirNumeroCasilla(tablero.getTablero(), x, y);
-							destaparCelda(x, y, numero);
-						} else if (SwingUtilities.isRightMouseButton(e)) {
-							comprobarBandera(x, y);
-						}
-					}
-				});
-
-				gbc.gridx = j;
-				gbc.gridy = i + 1;
-
-				panelInferior.add(tableroInterfaz[i][j], gbc);
-			}
-		}
+		gbc = generarEstructuraTablero();
+		generarTableroMinas();
 		setVisible(true);
 		pack();
 	}
 
 	public void tableroInicial(int i, int j) {
 		tableroInterfaz[i][j] = new JButton();
-		Color borde = new Color(0,0,0);
+		Color borde = new Color(0, 0, 0);
 		if (i == 0 && j == 0) {
-	        tableroInterfaz[i][j].setBorder(new MatteBorder(1, 1, 1, 1, borde));  // Para la primera celda (0,0)
-	    } else if (i == 0) {
-	        tableroInterfaz[i][j].setBorder(new MatteBorder(1, 0, 1, 1, borde));  // Para las celdas en la fila 0
-	    } else if (j == 0) {
-	        tableroInterfaz[i][j].setBorder(new MatteBorder(0, 1, 1, 1, borde));  // Para las celdas en la columna 0
-	    } else {
-	        tableroInterfaz[i][j].setBorder(new MatteBorder(0, 0, 1, 1, borde));  // Para el resto de las celdas
-	    }
-		ImageIcon icono = new ImageIcon("src/imagenes/celda0.gif");
-		Image imagen = icono.getImage().getScaledInstance(25, 25, Image.SCALE_SMOOTH);
-		icono = new ImageIcon(imagen);
-		icono.setDescription("celda0.gif");
-		tableroInterfaz[i][j].setIcon(icono);
+			tableroInterfaz[i][j].setBorder(new MatteBorder(1, 1, 1, 1, borde)); // Para la primera celda (0,0)
+		} else if (i == 0) {
+			tableroInterfaz[i][j].setBorder(new MatteBorder(1, 0, 1, 1, borde)); // Para las celdas en la fila 0
+		} else if (j == 0) {
+			tableroInterfaz[i][j].setBorder(new MatteBorder(0, 1, 1, 1, borde)); // Para las celdas en la columna 0
+		} else {
+			tableroInterfaz[i][j].setBorder(new MatteBorder(0, 0, 1, 1, borde)); // Para el resto de las celdas
+		}
+		actualizarCelda("celda0.gif",i,j);
 
 	}
-
+	
 	public void actualizarCelda(String fotoSrc, int i, int j) {
 		ImageIcon iconLogo = new ImageIcon("src/imagenes/" + fotoSrc);
 		Image image = iconLogo.getImage();
@@ -140,10 +133,9 @@ public class VentanaJuego extends JFrame {
 			} else {
 				actualizarCelda("sinMina.gif", x, y);
 			}
-			
+
 			tableroInterfaz[x][y].setDisabledIcon(tableroInterfaz[x][y].getIcon());
-		    tableroInterfaz[x][y].setEnabled(false);
-			
+			tableroInterfaz[x][y].setEnabled(false);
 
 			if (numero == 0) {
 				int[] calcularPosicionX = { -1, -1, -1, 0, 0, 1, 1, 1 };
@@ -163,8 +155,8 @@ public class VentanaJuego extends JFrame {
 			}
 		}
 	}
-	
-	public boolean condicionDestaparAgua (int nuevoX, int nuevoY) {
+
+	public boolean condicionDestaparAgua(int nuevoX, int nuevoY) {
 		return nuevoX >= 0 && nuevoX < tablero.getTablero().length && nuevoY >= 0
 				&& nuevoY < tablero.getTablero()[0].length && tableroInterfaz[nuevoX][nuevoY].isEnabled();
 	}
@@ -173,15 +165,15 @@ public class VentanaJuego extends JFrame {
 		actualizarCelda("explotada.gif", x, y);
 		tableroInterfaz[x][y].setDisabledIcon(tableroInterfaz[x][y].getIcon());
 		tableroInterfaz[x][y].setEnabled(false);
-		
+
 		for (int i = 0; i < tablero.getTablero().length; i++) {
 			for (int j = 0; j < tablero.getTablero()[i].length; j++) {
 				if (tablero.getTablero()[i][j].isMina() && (i != x && j != y)) {
 					actualizarCelda("mina.gif", i, j);
-					
 				}
+				
 				tableroInterfaz[i][j].setDisabledIcon(tableroInterfaz[i][j].getIcon());
-				tableroInterfaz[i][j].setEnabled(false); 
+				tableroInterfaz[i][j].setEnabled(false);
 			}
 		}
 	}
@@ -190,23 +182,76 @@ public class VentanaJuego extends JFrame {
 		Icon icono = tableroInterfaz[x][y].getIcon();
 		ImageIcon iconoImg = (ImageIcon) icono;
 		String ruta = iconoImg.getDescription();
-
+		
 		if (ruta.equals("celda0.gif")) {
 			actualizarCelda("bandera.gif", x, y);
+			this.minasRestantes--;
 		} else if (ruta.equals("bandera.gif")) {
 			actualizarCelda("interrogante.gif", x, y);
+			this.minasRestantes++;
 		} else {
 			actualizarCelda("celda0.gif", x, y);
 		}
+		int unidades = this.minasRestantes % 10;
+		int decenas = this.minasRestantes / 10;
+		actualizarContadorMinas(decenasMinas, decenas);
+		actualizarContadorMinas(unidadesMinas, unidades);
 	}
-	
+
 	public GridBagConstraints generarEstructuraTablero() {
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new Insets(0, 0, 0, 0);
 		gbc.fill = GridBagConstraints.NONE;
 		gbc.weightx = 1;
 		gbc.weighty = 1;
-		
+
 		return gbc;
+	}
+
+	public void generarTableroMinas() {
+		tablero.generarTablero();
+		tablero.colocarMinas(tablero.getTablero());
+		this.filas = tablero.getTablero().length;
+		this.columnas = tablero.getTablero()[0].length;
+		tableroInterfaz = new JButton[filas][columnas];
+		for (int i = 0; i < filas; i++) {
+			for (int j = 0; j < columnas; j++) {
+				tableroInicial(i, j);
+
+				final int x = i;
+				final int y = j;
+
+				tableroInterfaz[i][j].addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent e) {
+						if (SwingUtilities.isLeftMouseButton(e)) {
+							Icon icono = tableroInterfaz[x][y].getIcon();
+							ImageIcon iconoImg = (ImageIcon) icono;
+							String ruta = iconoImg.getDescription();
+							
+							if(!ruta.equals("bandera.gif")) {
+							int numero = tablero.conseguirNumeroCasilla(tablero.getTablero(), x, y);
+							destaparCelda(x, y, numero);
+							}
+						} else if (SwingUtilities.isRightMouseButton(e)) {
+							comprobarBandera(x, y);
+						}
+					}
+				});
+
+				this.gbc.gridx = j;
+				this.gbc.gridy = i + 1;
+				panelInferior.add(tableroInterfaz[i][j], gbc);
+			}
+		}
+	}
+
+	public void actualizarContadorMinas(JLabel cifra, int minas) {
+		String ruta = "time" + minas + ".gif";
+		
+		ImageIcon iconLogo = new ImageIcon("src/imagenes/" + ruta);
+		Image image = iconLogo.getImage();
+		Image scaledImage = image.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		iconLogo = new ImageIcon(scaledImage);
+		cifra.setIcon(iconLogo);
 	}
 }
